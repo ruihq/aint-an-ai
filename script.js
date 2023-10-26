62e149590c237769f88b68380f967e0da84386fd
Rui <120704613+ruihq@users.noreply.github.com>
2023-10-26 16:25:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const conversation = [];
+
 function askQuestion() {
     const question = document.getElementById('question').value;
     fetch(`https://hercai.onrender.com/v2/hercai?question="${question}"`)
@@ -6,10 +8,22 @@ function askQuestion() {
             const chatBody = document.getElementById('chat-body');
             const userMessage = `<div class="message user-message">${question}</div>`;
             const botMessage = `<div class="message bot-message">${JSON.stringify(data)}</div>`;
+            conversation.push({ user: question, bot: data });
             chatBody.innerHTML += userMessage + botMessage;
             chatBody.scrollTop = chatBody.scrollHeight; // Scroll to the bottom of the chat
         })
         .catch(error => {
             console.error('Error:', error);
         });
+}
+
+function displayConversation() {
+    const chatBody = document.getElementById('chat-body');
+    chatBody.innerHTML = '';
+    conversation.forEach(item => {
+        const userMessage = `<div class="message user-message">${item.user}</div>`;
+        const botMessage = `<div class="message bot-message">${JSON.stringify(item.bot)}</div>`;
+        chatBody.innerHTML += userMessage + botMessage;
+    });
+    chatBody.scrollTop = chatBody.scrollHeight;
 }
