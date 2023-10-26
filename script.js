02038150348380fe630709ae6c788391a89c2052
Rui <120704613+ruihq@users.noreply.github.com>
2023-10-26 16:21:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function askQuestion() {
     const question = document.getElementById('question').value;
-    fetch(`https://hercai.onrender.com/v2/hercai?question=${question}`)
+    fetch(`https://hercai.onrender.com/v2/hercai?question="${question}"`)
         .then(response => response.json())
         .then(data => {
-            document.getElementById('answer').textContent = data.answer;
+            const chatBody = document.getElementById('chat-body');
+            const userMessage = `<div class="message user-message">${question}</div>`;
+            const botMessage = `<div class="message bot-message">${JSON.stringify(data)}</div>`;
+            chatBody.innerHTML += userMessage + botMessage;
+            chatBody.scrollTop = chatBody.scrollHeight; // Scroll to the bottom of the chat
         })
         .catch(error => {
             console.error('Error:', error);
